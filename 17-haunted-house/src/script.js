845b3b8f6608d1b4b1b27ade7ad1d9b81c994ea9
Addison <shangxindi@gmail.com>
2023-05-28 22:43:43 +0800

    feat: complete house in 17 project

--- 17-haunted-house/src/script.js
+++ 17-haunted-house/src/script.js
@@ -23,13 +23,72 @@ const textureLoader = new THREE.TextureLoader()
 /**
  * House
  */
-// Temporary sphere
-const sphere = new THREE.Mesh(
-    new THREE.SphereGeometry(1, 32, 32),
-    new THREE.MeshStandardMaterial({ roughness: 0.7 })
+// Group
+const house = new THREE.Group()
+scene.add(house)
+
+// Walls
+const wallsPosition = {
+    x: 4,
+    y: 2.5,
+    z: 4
+}
+const walls = new THREE.Mesh(
+    new THREE.BoxGeometry(wallsPosition.x, wallsPosition.y, wallsPosition.z),
+    new THREE.MeshStandardMaterial({ color: '#ac8e82' })
 )
-sphere.position.y = 1
-scene.add(sphere)
+walls.position.y = wallsPosition.y / 2
+house.add(walls)
+
+// Door
+const doorPosition = {
+    x: 2,
+    y: 2,
+}
+const door = new THREE.Mesh(
+    new THREE.PlaneGeometry(doorPosition.x, doorPosition.y),
+    new THREE.MeshStandardMaterial({ color: '#aa7b7b' })
+)
+door.position.z = wallsPosition.z / 2 + 0.01 // 0.01 to avoid z-fighting
+door.position.y = doorPosition.y / 2
+house.add(door)
+
+// Bushes
+const bushGeometry = new THREE.SphereGeometry(1, 16, 16)
+const bushMaterial = new THREE.MeshStandardMaterial({ color: '#89c854' })
+
+const bush1 = new THREE.Mesh(bushGeometry, bushMaterial)
+bush1.scale.set(0.5, 0.5, 0.5)
+bush1.position.set(0.8, 0.2, 2.2)
+
+const bush2 = new THREE.Mesh(bushGeometry, bushMaterial)
+bush2.scale.set(0.25, 0.25, 0.25)
+bush2.position.set(1.4, 0.1, 2.1)
+
+const bush3 = new THREE.Mesh(bushGeometry, bushMaterial)
+bush3.scale.set(0.4, 0.4, 0.4)
+bush3.position.set(- 0.8, 0.1, 2.2)
+
+const bush4 = new THREE.Mesh(bushGeometry, bushMaterial)
+bush4.scale.set(0.15, 0.15, 0.15)
+bush4.position.set(- 1, 0.05, 2.6)
+
+house.add(bush1, bush2, bush3, bush4)
+
+// Roof
+// use a cone geometry to create a roof
+const roofPosition = {
+    x: 3.5,
+    y: 1,
+    z: 4
+}
+const roof = new THREE.Mesh(
+    new THREE.ConeGeometry(roofPosition.x, roofPosition.y, roofPosition.z),
+    new THREE.MeshStandardMaterial({ color: '#b35f45' })
+)
+roof.position.y = wallsPosition.y + roofPosition.y / 2
+roof.rotation.y = Math.PI / 4
+house.add(roof)
 
 // Floor
 const floor = new THREE.Mesh(
